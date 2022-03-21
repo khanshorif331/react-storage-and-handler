d9964e6e51b7889044c70305dc6de671a13198b6
khanshorif331 <khanshorif331@gmail.com>
2022-03-21 23:08:10 +0600

    data added in local storages

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,22 +1,20 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name,price,id} = props.cosmetic
 
     const addToCart =(id)=>{
-        console.log('item added',id);
+        addToDb(id)
     }
-
-    const addToCartWithParam = () =>addToCart(id)
 
     return (
         <div className='product'>
             <h2>Buy This: {name}</h2>
             <p>Only For: {price}</p>
             <p><small>It has id: {id}</small></p>
-            <button onClick={addToCartWithParam}>Add to cart</button>
-            <button onClick={()=>addToCart(id)}>Purchase</button>
+            <button onClick={()=>addToCart(id)}>Add to cart </button>
         </div>
     );
 };
